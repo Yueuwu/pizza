9b26847e57b7b582cdeaf96dbb9ac5436ba9ac6f
Yueuwu <vladdenega1@gmail.com>
2022-10-28 12:27:50 +0700

    future: added lazy loading for the Cart page

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import './scss/app.scss'
 import Header from "./components/Header";
 import Home from "./components/pages/Home";
 import {Route, Routes} from "react-router-dom";
-import Cart from "./components/pages/Cart";
 import NotFound from "./components/pages/NotFound";
 import ExactPizza from "./components/pages/ExactPizza/ExactPizza";
+import React from "react";
+import Loader from "./components/Loader/Loader";
+
+const Cart = React.lazy(() => import(/*webpackChunkName: "Cart"*/ "./components/pages/Cart"))
 
 function App() {
 
@@ -15,7 +18,11 @@ function App() {
                 <div className="content">
                     <Routes>
                         <Route path='/'  element={<Home/>}/>
-                        <Route path='/cart' element={<Cart />}/>
+                        <Route path='/cart' element={
+                            <React.Suspense fallback={<div className='loader'><Loader/></div>}>
+                                <Cart />
+                            </React.Suspense>
+                        }/>
                         <Route path='/pizza/:id' element={<ExactPizza/>}/>
                         <Route path='*' element={<NotFound />}/>
                     </Routes>
--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import style from './style.module.css'
 
-const Loader = () => {
+const Loader: React.FC = () => {
     return (
         <div className={style.ldsRoller}>
             <div></div>
